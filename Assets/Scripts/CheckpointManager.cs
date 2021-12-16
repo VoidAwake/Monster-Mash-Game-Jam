843e8a1acf4c5127fc1adc794bf1a6defc2c37ad
A-G-K <46753947+A-G-K@users.noreply.github.com>
2021-12-16 16:54:27 +1100

    Yes

---
+++ Assets/Scripts/CheckpointManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using RoboRyanTron.Unite2017.Events;
+using UnityEngine;
+
+public class CheckpointManager : MonoBehaviour
+{
+    private static CheckpointManager _instance;
+
+    public static CheckpointManager Instance { get { return _instance; } }
+
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        } else {
+            _instance = this;
+        }
+    }
+
+    [SerializeField] private GameObject player;
+    
+    [SerializeField] private CheckpointObj currCheckpoint;
+
+    [ContextMenu("PlayerReset")]
+    public void OnDeath()
+    {
+        if (currCheckpoint != null) player.transform.position = currCheckpoint.transform.position;
+    }
+    
+    public void OnCheckpointReach(CheckpointObj checkpointObj)
+    {
+        currCheckpoint = checkpointObj;
+    }
+}
