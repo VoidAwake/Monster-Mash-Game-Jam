80693e630515fce575072ffc8e5b36c96a6f8087
Jack Coggins <voidawake90@gmail.com>
2021-12-17 14:43:00 +1100

    Hook up oxygen to movement

--- Assets/Scripts/OxygenMeter.cs
+++ Assets/Scripts/OxygenMeter.cs
@@ -7,10 +7,25 @@ using UnityEngine.UI;
 public class OxygenMeter : MonoBehaviour
 {
     private Slider slider;
+    private OxygenTank oxygenTank;
 
     private void Awake()
     {
         slider = GetComponent<Slider>();
+    }
+
+    private void Start()
+    {
+        oxygenTank = FindObjectOfType<OxygenTank>();
+
+        oxygenTank.oxygenLevelChanged.AddListener(OnOxygenLevelChanged);
+
+        OnOxygenLevelChanged();
+    }
+
+    private void OnOxygenLevelChanged()
+    {
+        UpdateOxygen(oxygenTank.OxygenLevel / oxygenTank.maxOxygenLevel * 100);
     }
 
     public void UpdateOxygen(float value)
